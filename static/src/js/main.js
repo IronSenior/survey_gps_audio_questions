ce1ad676143f042e6a24f0561867d7ce32d4a04e
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2020-04-25 16:52:58 +0200

    Js file started

---
+++ static/src/js/main.js
@@ -0,0 +1,11 @@
+odoo.define('survey_asl_question.survey', function (require) {
+    'use strict';
+
+    require('web.dom_ready');
+    var ajax = require('web.ajax');
+    var core = require('web.core');
+
+    var _t = core._t;
+
+
+});
