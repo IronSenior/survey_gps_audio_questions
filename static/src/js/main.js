0426c951743fe8458169cd2aa9b195e6c466577f
Sergio Gómez <sergio@uco.es>
2020-04-25 18:23:32 +0200

    Add read GPS button

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,11 +1,24 @@
-odoo.define('survey_asl_question.survey', function (require) {
-    'use strict';
+odoo.define("survey_asl_question.survey", function (require) {
+  "use strict";
 
-    require('web.dom_ready');
-    var ajax = require('web.ajax');
-    var core = require('web.core');
+  require("web.dom_ready");
+  var ajax = require("web.ajax");
+  var core = require("web.core");
 
-    var _t = core._t;
+  var _t = core._t;
 
+  $(document).ready(function () {
+    var readGPSButton = document.getElementById("read_gps_button");
+    if (readGPSButton) {
+      readGPSButton.addEventListener("click", readGPS);
+    }
+  });
 
+  function readGPS() {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      var input = document.getElementById("response_gps_button");
+
+      input.value = `${location.coords.latitude},${location.coords.longitude},${location.coords.accuracy}`;
+    });
+  }
 });
