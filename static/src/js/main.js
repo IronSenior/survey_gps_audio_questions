c12bb303b13bb6080d223261243c7b1a444e9457
Sergio Gómez <sergio@uco.es>
2020-04-26 23:46:12 +0200

    Add progress bar in audio recording

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -32,16 +32,86 @@ odoo.define("survey_asl_question.survey", function (require) {
     return this;
   };
 
-  var recorder;
-  var recordAudioButton;
-  var recordAudioInput;
-  var recordDuration;
+  $("#aulasl_audio_button").exists(function () {
+    const audioButton = $("#aulasl_audio_button");
+    const audioDuration = audioButton.data("audio_duration");
+    let audioRecorder;
 
-  recordAudioButton = document.getElementById("read_audio_button");
-  recordAudioInput = document.getElementById("read_audio_input");
-  if (recordAudioButton && recordAudioInput) {
-    recordAudioButton.addEventListener("click", recordAudio);
-  }
+    audioButton.on("click", function () {
+      console.debug("button audio");
+      audioButton.prop("disabled", true);
+
+      const refreshProgress = function (current) {
+        const progress = $("#aulasl_audio_progress");
+
+        if (current >= audioDuration) {
+          progress.css("width", "100%");
+
+          return;
+        }
+
+        const width = (current / audioDuration) * 100;
+        progress.css("width", `${width}%`);
+
+        setTimeout(refreshProgress.bind(null, current + 1000), 1000);
+      };
+
+      const save = function (blob) {
+        const reader = new FileReader();
+
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+          const base64Audio = reader.result.split(",")[1];
+          $("#aulasl_audio_input").val(base64Audio);
+          $("#aulasl_audio_alert").message("Grabación terminada");
+        };
+      };
+
+      const stop = function (stream) {
+        audioButton.prop("disabled", false);
+        audioRecorder.stop();
+        stream.getAudioTracks()[0].stop();
+        audioRecorder.exportWAV(save);
+      };
+
+      const success = function (stream) {
+        const audioContext = new AudioContext();
+        const input = audioContext.createMediaStreamSource(stream);
+
+        audioRecorder = new Recorder(input, { numChannels: 1 });
+        audioRecorder.record();
+
+        setTimeout(stop.bind(null, stream), audioDuration);
+        setTimeout(refreshProgress.bind(null, 0), 0);
+      };
+
+      const error = function (error) {
+        audioButton.prop("disabled", false);
+
+        if (
+          error.name === "PermissionDeniedError" ||
+          error.name === "NotAllowedError"
+        ) {
+          $("#aulasl_audio_alert").message(
+            "Error al acceder al micrófono. Deberá otorgar permisos para continuar",
+            true
+          );
+
+          return;
+        }
+
+        $("#aulasl_audio_alert").message(
+          "Error desconocido al acceder al micrófono: " + error.name,
+          true
+        );
+      };
+
+      navigator.mediaDevices
+        .getUserMedia({ audio: true, video: false })
+        .then(success)
+        .catch(error);
+    });
+  });
 
   $("#aulasl_gps_button").exists(function () {
     if (!navigator.geolocation) {
@@ -90,54 +160,4 @@ odoo.define("survey_asl_question.survey", function (require) {
       navigator.geolocation.getCurrentPosition(success, error);
     });
   });
-
-  function saveAudio(blob) {
-    var url = URL.createObjectURL(blob);
-    var reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = function () {
-      var base64Audio = reader.result;
-      recordAudioInput.value = base64Audio.split(",")[1];
-    };
-  }
-
-  function stopRecordAudio(stream) {
-    recordAudioButton.disabled = false;
-    recorder.stop();
-    stream.getAudioTracks()[0].stop();
-    recorder.exportWAV(saveAudio);
-  }
-
-  function recordCountdown(time) {
-    if (time <= 0) {
-      $(".js_infozone").hide();
-
-      return;
-    }
-
-    $(".js_infozone").show();
-    $(".js_infozone").text(`Quedan ${time / 1000} segundos`);
-
-    setTimeout(recordCountdown.bind(null, time - 1000), 1000);
-  }
-
-  function recordAudio() {
-    recordAudioButton.disabled = true;
-    recordDuration = $("#read_audio_input").data("audio_duration");
-
-    navigator.mediaDevices
-      .getUserMedia({ audio: true, video: false })
-      .then(function (stream) {
-        var audioContext = new AudioContext();
-        var input = audioContext.createMediaStreamSource(stream);
-        recorder = new Recorder(input, { numChannels: 1 });
-        recorder.record();
-
-        setTimeout(stopRecordAudio.bind(null, stream), recordDuration);
-        setTimeout(recordCountdown.bind(null, recordDuration), 0);
-      })
-      .catch(function (err) {
-        recordAudioButton.disabled = false;
-      });
-  }
 });
