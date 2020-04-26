05b25aa74988385ae8aa7d1281676578de18bd44
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2020-04-26 21:21:54 +0200

    Set and get for res config settings

--- models/res_config.py
+++ models/res_config.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResConfigSettings(models.TransientModel):
@@ -8,3 +8,20 @@ class ResConfigSettings(models.TransientModel):
     google_api_key = fields.Char(
         string = "Google Api Key"
     )
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            google_api_key = self.env['ir.config_parameter'].sudo().get_param('survey_asl_question.google_api_key'),
+        )
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        param = self.env['ir.config_parameter'].sudo()
+
+        api_key = self.google_api_key
+
+        param.set_param('survey_asl_question.google_api_key', api_key)
